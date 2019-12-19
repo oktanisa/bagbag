400a1444e02c1b0cb4c538c8c603dd1c355e7cb3
oktanisa <oktanisa19@users.noreply.github.com>
2019-12-19 16:23:45 +0700

    1555
    1t

---
+++ checkout.php
@@ -0,0 +1,343 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+   
+    <!--====== Required meta tags ======-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!--====== Title ======-->
+    <title>@BAGBAG</title>
+    
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="assets/images/tas.webp" type="image/webp">
+
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    
+    <!--====== Animate css ======-->
+    <link rel="stylesheet" href="assets/css/animate.css">
+    
+    <!--====== Magnific Popup css ======-->
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    
+    <!--====== Slick css ======-->
+    <link rel="stylesheet" href="assets/css/slick.css">
+    
+    <!--====== Line Icons css ======-->
+    <link rel="stylesheet" href="assets/css/LineIcons.css">
+    
+    <!--====== Default css ======-->
+    <link rel="stylesheet" href="assets/css/default.css">
+    
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+    <!--====== Responsive css ======-->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    
+   <!--====== css form ======-->
+    <!-- Animate.css -->
+
+	
+	
+	
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="assets/css2/style2.css">
+
+	<!-- Modernizr JS -->
+	<script src="assets/js2/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	
+  
+</head>
+ 
+    <!--====== PRELOADER PART START ======-->
+	
+  
+    <!--====== PRELOADER PART START ======-->
+    
+    <!--====== HEADER PART START ======-->
+    
+    <header class="header-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg">
+                        <a class="navbar-brand" href="index.html">
+                            <img src="assets/images/LOGOnew.png" width="150px" heigth="50px" alt="Logo">
+                        </a> <!-- Logo -->
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="bar-icon"></span>
+                            <span class="bar-icon"></span>
+                            <span class="bar-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul id="nav" class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a data-scroll-nav="0" href="#home">Beranda</a>
+                                </li>
+                                <li class="nav-item">
+                                    <div class="dropdown">
+                                        <a class=" dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            Produk</a>
+                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                                <a class="dropdown-item" href="#">Sport Bra</a>
+                                                <a class="dropdown-item" href="#">Tas</a>
+                                                <a class="dropdown-item" href="#">Korset</a>
+                                                <a class="dropdown-item" href="#">Perlengkapan Bayi</a>
+                                            </div>
+                                    </div>
+                                </li>
+                                        
+                                      
+                                <li class="nav-item">
+                                    <a data-scroll-nav="0" href="keranjang.php">Keranjang</a>
+                                </li>
+                            </ul> <!-- navbar nav -->
+                        </div>
+                    </nav> <!-- navbar -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </header>
+    
+    <!--====== HEADER PART ENDS ======-->
+    <body class="colorlib">
+		<div class="colorlib-shop">
+			<div class="container">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+				<div class="row row-pb-md">
+					<div class="col-md-12 col-md-offset-1">
+						<div class="process-wrap">
+							<div class="process text-center active">
+								<p><span>01</span></p>
+								<h3>Shopping Cart</h3>
+							</div>
+							<div class="process text-center active">
+								<p><span>02</span></p>
+								<h3>Checkout</h3>
+							</div>
+							<div class="process text-center">
+								<p><span>03</span></p>
+								<h3>Order Complete</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-7">
+						<form method="post" class="colorlib-form">
+							<h2>Rincian Pesanan</h2>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <div class="form-field">
+                                           <i class="icon icon-arrow-down3"></i>        
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-md-12">
+                                        <label for="fname">Nama Depan</label>
+                                        <input type="text" id="fname" class="form-control" placeholder="Nama Depan">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="lname">Nama Belakang</label>
+                                        <input type="text" id="lname" class="form-control" placeholder="Nama Belakang">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="companyname">Provinsi</label>
+                                        <input type="text" id="companyname" class="form-control" placeholder="Provinsi">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="fname">Kabupaten/Kota</label>
+                                        <input type="text" id="Kabupaten/Kota" class="form-control" placeholder="Kabupaten/Kota">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="companyname">Kecamatan</label>
+                                        <input type="text" id="towncity" class="form-control" placeholder="Kecamatan">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="stateprovince">Jalan/Blok/No.Rumah/RT/RW</label>
+                                        <input type="text" id="fname" class="form-control" placeholder="Jalan/Blok/No.Rumah/RT/RW">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="lname">Kode Pos</label>
+                                        <input type="text" id="zippostalcode" class="form-control" placeholder="Kode Pos">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="email">Email</label>
+                                        <input type="text" id="email" class="form-control" placeholder="Email">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <label for="Phone">No Tlp / WA</label>
+                                        <input type="text" id="zippostalcode" class="form-control" placeholder="No handphone/WA">
+                                    </div>
+                                </div>
+                            </div>  
+		                </form>
+					</div>
+					<div class="col-md-5">
+						<div class="cart-detail">
+							<h2>Total Belanja</h2>
+							<ul>
+								<li>
+									<span>Subtotal</span> <span>$100.00</span>
+									<ul>
+										<li><span>1 x Product Name</span> <span>$99.00</span></li>
+										<li><span>1 x Product Name</span> <span>$78.00</span></li>
+									</ul>
+								</li>
+								<li><span>Ongkir</span> <span>$0.00</span></li>
+								<li><span>Total Pesanan</span> <span>$180.00</span></li>
+							</ul>
+						</div>
+						<div class="cart-detail">
+							<h2>Pembayaran</h2>
+							<img src="assets/images/rekening.png" height="200px" >
+						</div>	
+						<br><br>
+						<div class="row">
+							<div class="col-md-12">
+								<p><a href="pembayaran.php" class="btn btn-primary">Upload Bukti Transfer</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+    </body>
+	<!--====== FOOTER PART START ======-->
+    
+<section id="footer" class="footer-area">
+        <div class="container">
+            <div class="footer-widget pt-75 pb-120">
+                <div class="row">
+                    <div class="col-lg-6 col-md-7 col-sm-7">
+                        <div class="footer-logo mt-40">
+                            <a href="#">
+                            <img src="assets/images/LOGOnew.png" width="150px" heigth="50px" alt="Logo">
+                            </a>
+                            <p class="mt-14">Toko online terlengkap dan termurah dengan harga promo setiap harinya.
+                                Tidak hanya produk bra saja, kami juga menyediakan perlengkapan bayi, tas dan korset.</p>
+                        </div> <!-- footer logo -->
+                    </div>
+                    <div class="col-lg-2 col-md-4 col-sm-5">
+                        <div class="footer-link mt-50">
+                            <h5 class="f-title">Produk</h5>
+                            <ul>
+                                <li><a href="#">Sport Bra</a></li>
+                                <li><a href="#">Tas</a></li>
+                                <li><a href="#">Korset</a></li>
+                                <li><a href="#">Perlengkapan Bayi</a></li>
+                            </ul>
+                        </div> <!-- footer link -->
+                    </div>
+                    <div class="col-lg-4 col-md-5 col-sm-7">
+                        <div class="footer-info mt-50">
+                            <h5 class="f-title">Informasi Kontak</h5>
+                            <ul>
+                                <li>
+                                    <div class="single-footer-info mt-20">
+                                        <span>Telepon :</span>
+                                        <div class="footer-info-content">
+                                            <p>(0331) 330900</p>
+                                            
+                                        </div>
+                                    </div> <!-- single footer info -->
+                                </li>
+                                <li>
+                                    <div class="single-footer-info mt-20">
+                                        <span>Email :</span>
+                                        <div class="footer-info-content">
+                                            <p>abagbag@gmail.com</p>
+                                            
+                                        </div>
+                                    </div> <!-- single footer info -->
+                                </li>
+                                <li>
+                                    <div class="single-footer-info mt-20">
+                                        <span>Alamat :</span>
+                                        <div class="footer-info-content">
+                                            <p>Jl. S.Parman No.42 Jember</p>
+                                        </div>
+                                    </div> <!-- single footer info -->
+                                </li>
+                            </ul>
+                        </div> <!-- footer link -->
+                    </div>
+                </div> <!-- row -->
+            </div> <!-- footer widget -->
+            <div class="footer">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="copyright text-center">
+                            <p>Copyright by <a href="https://abagbag.com" rel="nofollow">@bagbag</a></p>
+                        </div> <!-- copyright -->
+                    </div>
+                </div> <!-- row -->
+            </div> <!--  footer copyright -->
+        </div> <!-- container -->
+</section>
+    
+    <!--====== FOOTER PART ENDS ======-->
+    
+    <!--====== BACK TO TOP PART START ======-->
+    
+    <a href="#" class="back-to-top"><i class="lni-chevron-up"></i></a>
+    
+    <!--====== BACK TO TOP PART ENDS ======-->
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <!--====== jquery js ======-->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+    
+    <!--====== Slick js ======-->
+    <script src="assets/js/slick.min.js"></script>
+    
+    <!--====== Magnific Popup js ======-->
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    
+    <!--====== nav js ======-->
+    <script src="assets/js/jquery.nav.js"></script>
+    
+    <!--====== Nice Number js ======-->
+    <script src="assets/js/jquery.nice-number.min.js"></script>
+    
+    <!--====== Main js ======-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
